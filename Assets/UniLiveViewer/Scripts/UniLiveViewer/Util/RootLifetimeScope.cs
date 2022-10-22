662f37d9153df47eedcdf8672de4ad775754a097
mocatea <86557651+mocatea@users.noreply.github.com>
2022-10-22 22:43:44 +0900

    VContainerの環境準備

---
+++ Assets/UniLiveViewer/Scripts/UniLiveViewer/Util/RootLifetimeScope.cs
@@ -0,0 +1,10 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+public class RootLifetimeScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+
+    }
+}
